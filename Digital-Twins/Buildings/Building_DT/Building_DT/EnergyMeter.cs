ab55c9a7b4048bfbc49f82a1f19f77380375ec2e
GabeDS9 <gabrieldasilva01@gmail.com>
2022-03-08 16:20:43 +0200

    Digital Twins commit
    Committed the initial code for DT development

---
+++ Digital-Twins/Buildings/Building_DT/Building_DT/EnergyMeter.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building_DT
+{
+    class EnergyMeter
+    {
+        // Energy Meter Data initialisation
+        public string meterid { get; set; }
+        public string description { get; set; }
+        public string make { get; set; }
+        public string type { get; set; }
+        public string serial_no { get; set; }
+        public string yard_no { get; set; }
+        public string building_no { get; set; }
+        public string floor { get; set; }
+        public string room_no { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public List<EnergyData> data { get; set; }
+
+        private APICaller apiCaller = new APICaller();
+
+        private string prev_Date = "";
+
+        // Get energy meter historic data
+        public void GetEnergyUsage()
+        {
+            var apikey = "68b408399bdcbf3d5d4b3485c76596e8015c9f797414a83e3aa626d04d070abe"; //"[YOUR API KEY HERE]";
+            var from_date = "2022-02-01%2000:00:00";
+            var to_date = GetCurrentDateTime();
+            prev_Date = to_date;
+            var interval = "ts_5min";
+
+            var url = $"https://api.indivo.co.za/Energy/EnergyData?id={meterid}&from_date={from_date}&to_date={to_date}&interval={interval}&key={apikey}";
+
+            var task = apiCaller.CallEnergyAPI(url);
+
+            if(task.Result != null)
+            {
+                EnergyMeter meterdata = JsonConvert.DeserializeObject<EnergyMeter>(task.Result);
+                data = meterdata.data;
+
+                if(data != null)
+                {
+                    if(data.Count > 0)
+                    {
+                        Console.WriteLine(meterdata.meterid + " " + meterdata.description + ": " + data[data.Count - 1].ptot_kw + " - Timestamp: " + data[data.Count - 1].timestamp);
+                    }
+                }
+            }
+            else
+            {
+                data = null;
+                Console.WriteLine("No data");
+            }
+        }
+
+        // Get current energy meter reading
+        public void GetCurrentEnergyUsage()
+        {
+            var apikey = "68b408399bdcbf3d5d4b3485c76596e8015c9f797414a83e3aa626d04d070abe"; //"[YOUR API KEY HERE]";
+            var from_date = prev_Date;
+            var to_date = GetCurrentDateTime();
+
+            var interval = "ts_5min";
+
+            var url = $"https://api.indivo.co.za/Energy/EnergyData?id={meterid}&from_date={from_date}&to_date={to_date}&interval={interval}&key={apikey}";
+
+            var task = apiCaller.CallEnergyAPI(url);
+
+            EnergyMeter meterdata = JsonConvert.DeserializeObject<EnergyMeter>(task.Result);
+
+            if (task != null && (meterdata.data.Count > 1))
+            {
+                if (data[data.Count - 1].timestamp != meterdata.data[meterdata.data.Count - 1].timestamp)
+                {
+
+                    data.Add(meterdata.data[meterdata.data.Count - 1]);
+                    Console.WriteLine("Current Energy Data has been added to " + meterdata.description + " at " + data[data.Count - 1].timestamp);
+                    prev_Date = to_date;
+                }
+                else
+                {
+                    Console.WriteLine("No current Energy Data has been added to " + meterdata.description);
+                }
+            }
+            
+        }
+
+        // Gives current date and time for current meter readings
+        private string GetCurrentDateTime()
+        {
+            string date = "";
+            DateTime currDate = DateTime.Now;
+
+            date = currDate.Year + "-" + currDate.Month + "-" + currDate.Day + "%20" + currDate.Hour + ":" + currDate.Minute + ":00";
+
+            return date;
+        }
+    }
+}
---
+++ Digital-Twins/Buildings/Building_DT/Building_DT/SocketServer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// Socket Listener acts as a server and listens to the incoming
+// messages on the specified port and protocol.
+public class SocketServer
+{
+    Socket handler = null;
+
+    public void StartServer()
+    {
+        // Get Host IP Address that is used to establish a connection
+        // In this case, we get one IP address of localhost that is IP : 127.0.0.1
+        // If a host has multiple addresses, you will get a list of addresses
+        IPHostEntry host = Dns.GetHostEntry("localhost");
+        IPAddress ipAddress = host.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+        try
+        {
+
+            // Create a Socket that will use Tcp protocol
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            // A Socket must be associated with an endpoint using the Bind method
+            listener.Bind(localEndPoint);
+            // Specify how many requests a Socket can listen before it gives Server busy response.
+            // We will listen 10 requests at a time
+            listener.Listen(10);
+
+            Console.WriteLine("Waiting for a connection...");
+            handler = listener.Accept();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    public void SendMessage(string data)
+    {
+        byte[] msg = Encoding.ASCII.GetBytes(data);
+        handler.Send(msg);
+    }
+
+    public string ReceiveMessage()
+    {
+        // Incoming data from the client.
+        string data = null;
+        byte[] bytes = null;
+
+        while (true)
+        {
+            bytes = new byte[1024];
+            int bytesRec = handler.Receive(bytes);
+            data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+            if (data.IndexOf("Send meter list") > -1)
+            {
+                return "MeterList requested";
+            }
+            if(data.IndexOf("Next") > -1)
+            {
+                return "Next Meter";
+            }
+        }
+    }
+
+    public void CloseServer()
+    {
+        handler.Shutdown(SocketShutdown.Both);
+        handler.Close();
+    }
+}
