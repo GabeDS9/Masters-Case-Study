63bc939172b9c34b74ee8ec409f5140def4b4f35
GabeDS9 <62903035+GabeDS9@users.noreply.github.com>
2022-04-04 16:27:38 +0200

    Created services for DTs project

---
+++ Digital-Twins/Services/DT_Services/DT_Services/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DT_Services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
