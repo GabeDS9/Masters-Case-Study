e9b38179922ce7f2631dc278142211e08189dede
GabeDS9 <20935927@sun.ac.za>
2022-05-24 09:53:23 +0200

    Configuration File changes for testing

--- Unity-Environments/VR_DT_CaseStudy/Assets/Scripts/Menu/MenuManager.cs
+++ Unity-Environments/VR_DT_CaseStudy/Assets/Scripts/Menu/MenuManager.cs
@@ -276,11 +276,8 @@ public class MenuManager : MonoBehaviour
     }
     public void ChangeVisualisationDate()
     {
-        if (visualisationSlider.value != 0)
-        {
             var date = utils.GenerateDateList(StartDateSelected, EndDateSelected, TimePeriodSelected)[((int)visualisationSlider.value)];
             mapSpawnner.ChangeVisualisationDate(date);
-        }
     }
     public void ChangeVisualisationScale()
     {
@@ -389,23 +386,19 @@ public class MenuManager : MonoBehaviour
         StartCalendar.SetActive(true);
         startDate.text = "";
     }
-
     public void DisableStartCalendar()
     {
         StartCalendar.SetActive(false);
     }
-
     public void DisplayEndCalendar()
     {
         EndCalendar.SetActive(true);
         endDate.text = "";
     }
-
     public void DisableEndCalendar()
     {
         EndCalendar.SetActive(false);
-    }
-    public void ChangeDisplayLevelNext()
+    }    public void ChangeDisplayLevelNext()
     {
         DisplayLevelNextButton.interactable = true;
     }
@@ -416,215 +409,4 @@ public class MenuManager : MonoBehaviour
             DateSelectNextButton.interactable = true;
         }        
     }
-    /*#region EnergyFields
-    public void EnergyMeters()
-    {
-        MeterMenu.SetActive(true);
-        currentMenu = MeterMenu;
-        MainMenu.SetActive(false);
-        PopulateEnergyDropdown(appManager.EnergyMeterList);
-    }
-    public void PopulateEnergyDropdown(List<EnergyMeterData> EnergyMeterList)
-    {
-        List<String> options = new List<String>();
-
-        foreach(var item in EnergyMeterList)
-        {
-            options.Add(item.meterid.ToString());
-        }
-
-        MeterListDropDown.AddOptions(options);
-    }
-
-    public void SpawnEnergyObjects(string date, List<EnergyAverage> averageList, string visualisation_type)
-    {
-        mapSpawnner.PopulateEnergyObject(tempEnergyMeter, averageList, date, visualisation_type);
-    }
-
-    #endregion
-
-    #region OccupancyFields
-    public void OccupancyMeters()
-    {
-        MeterMenu.SetActive(true);
-        currentMenu = MeterMenu;
-        MainMenu.SetActive(false);
-        PopulateOccupancyDropdown(appManager.OccupancyMeterList);
-    }
-    public void PopulateOccupancyDropdown(List<OccupancyMeterData> OccupancyMeterList)
-    {
-        List<String> options = new List<String>();
-
-        foreach (var item in OccupancyMeterList)
-        {
-            options.Add(item.meterid.ToString());
-        }
-
-        MeterListDropDown.AddOptions(options);
-    }
-
-    public void SpawnOccupancyObjects(string date, List<EnergyAverage> averageList, string visualisation_type)
-    {
-        mapSpawnner.PopulateEnergyObject(tempEnergyMeter, averageList, date, visualisation_type);
-    }
-    #endregion
-
-    #region SolarFields
-    public void SolarMeters()
-    {
-        MeterMenu.SetActive(true);
-        currentMenu = MeterMenu;
-        MainMenu.SetActive(false);
-        PopulateSolarDropdown(appManager.SolarMeterList);
-    }
-    public void PopulateSolarDropdown(List<SolarMeterData> SolarMeterList)
-    {
-        List<String> options = new List<String>();
-
-        foreach (var item in SolarMeterList)
-        {
-            options.Add(item.meterid.ToString());
-        }
-
-        MeterListDropDown.AddOptions(options);
-    }
-
-    public void SpawnSolarObjects(string date, List<EnergyAverage> averageList, string visualisation_type)
-    {
-        mapSpawnner.PopulateEnergyObject(tempEnergyMeter, averageList, date, visualisation_type);
-    }
-    #endregion
-
-    #region Visualisations
-
-    public void VisualisationsMenu()
-    {
-        currentMenu.SetActive(false);
-        VisualisationMenu.SetActive(true);
-        currentMenu = VisualisationMenu;
-        VisualisationDataCalculations();
-        dayVisualisationButton.enabled = visIsReady;
-        monthVisualisationButton.enabled = visIsReady;
-        readyIndicator.text = "retrieving information....";
-    }
-
-    private async void VisualisationDataCalculations()
-    {
-        String startdate = startDate.text;
-        String enddate = endDate.text;
-        meterid = Int32.Parse(MeterListDropDown.options[MeterListDropDown.value].text);
-
-        Debug.Log("Obtaining data from " + startdate + " to " + enddate + " for " + meterid);
-
-        await Task.Run(() => appManager.energyManager.CalculateDayAverage(startdate, enddate, meterid));
-
-        await Task.Run(() => appManager.energyManager.CalculateMonthAverage(startdate, enddate, meterid));
-
-        foreach (var item in appManager.energyManager.EnergyMeters)
-        {
-            if (item.meterid == meterid)
-            {
-
-                tempEnergyMeter = item;
-
-                /*foreach (var res in item.day_average)
-                {
-                    Debug.Log($"Day average for {meterid} on {res.timestamp} is {res.ptot_kw}");
-                }
-
-                foreach (var res in item.month_average)
-                {
-                    Debug.Log($"Month average for {meterid} on {res.timestamp} is {res.ptot_kw}");
-                }
-
-                break;
-            }
-        }
-
-        readyIndicator.text = "Visualisation ready";
-        visIsReady = true;
-        dayVisualisationButton.enabled = visIsReady;
-        monthVisualisationButton.enabled = visIsReady;
-        Debug.Log("Information is obtained");
-    }
-
-    public void DayVisualisation()
-    {
-        currentMenu.SetActive(false);
-        VisualisationUI.SetActive(true);
-        currentMenu = VisualisationUI;
-        isDayButtonPressed = true;
-
-        foreach (var item in appManager.energyManager.EnergyMeters)
-        {
-            if (item.meterid == meterid)
-            {
-                visualisationSlider.maxValue = item.day_average.Count - 1;
-                Debug.Log("Slider should have " + item.day_average.Count);
-                dayAverage = item.day_average;
-                break;
-            }
-        }
-
-        SpawnEnergyObjects(dayAverage[0].timestamp, dayAverage, "day_visualisation");
-                 
-    }
-
-    public void MonthVisualisation()
-    {
-        currentMenu.SetActive(false);
-        VisualisationUI.SetActive(true);
-        currentMenu = VisualisationUI;
-        isMonthButtonPressed = true;
-
-        foreach (var item in appManager.energyManager.EnergyMeters)
-        {
-            if (item.meterid == meterid)
-            {
-                visualisationSlider.maxValue = item.month_average.Count - 1;
-                Debug.Log("Slider should have " + item.month_average.Count);
-                monthAverage = item.month_average;
-                break;
-            }
-        }
-
-        SpawnEnergyObjects(monthAverage[0].timestamp, monthAverage, "month_visualisation");
-
-    }
-
-    public void ChangeSlider()
-    {
-        if (isDayButtonPressed)
-        {
-            SpawnEnergyObjects(dayAverage[(int)visualisationSlider.value].timestamp, dayAverage, "day_visualisation");
-        }
-        else if (isMonthButtonPressed)
-        {
-            SpawnEnergyObjects(monthAverage[(int)visualisationSlider.value].timestamp, monthAverage, "month_visualisation");
-        }
-    }
-
-    #endregion
-
-    public void DisplayStartCalendar()
-    {
-        StartCalendar.SetActive(true);
-    }
-
-    public void DisableStartCalendar()
-    {
-        StartCalendar.SetActive(false);
-    }
-
-    public void DisplayEndCalendar()
-    {
-        EndCalendar.SetActive(true);
-    }
-
-    public void DisableEndCalendar()
-    {
-        EndCalendar.SetActive(false);
-    }*/
-
-
 }
